ee428d45763f23e359b2712913f6245ab9b42f0c
Wosugam <waldo2806@gmail.com>
2025-06-11 19:51:41 -0400

    C: Added movement to the right and left

--- main.py
+++ main.py
@@ -7,23 +7,20 @@ def main():
     fps = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    main_player = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))  
+    main_player = Player(int(SCREEN_WIDTH/2), int(SCREEN_HEIGHT/2))  
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
+        main_player.update(dt)
+            
         screen.fill((0,0,0))
         main_player.draw(screen)
         pygame.display.flip()
 
         dt = fps.tick(60)/1000
-
-        
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
